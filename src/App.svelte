92a5e3c1697abc1a3d2fec7ba7d5317be4262c72
Ivan Savitsky <ivan@savitsky.biz>
2020-04-22 15:12:42 +0300

    Add poll list

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Header from './components/Header.svelte';
 	import Footer from './components/Footer.svelte';
+	import PollList from './components/PollList.svelte';
 	import CreatePollForm from './components/CreatePollForm.svelte';
 	import Tabs from './shared/Tabs.svelte';
 
@@ -10,15 +11,33 @@
 	const tabChange = (e) => {
 		activeItem = e.detail;
 	};
+
+	let polls = [
+		{
+			id: 1,
+			question: 'JavaScript or TypeScript?',
+			answerA: 'JavaScript',
+			answerB: 'TypeScript',
+			votesA: 9,
+			votesB: 15
+		}
+	];
+
+	const handleAdd = (e) => {
+		const poll = e.detail;
+		polls = [poll, ...polls];
+		console.log(polls);
+		activeItem = 'Current Polls';
+	}
 </script>
 
 <Header />
 <main>
 	<Tabs {items} {activeItem} on:tabChange={tabChange}/>
 	{#if activeItem === 'Current Polls'}
-		<p>Poll list component</p>
+		<PollList {polls} />
 	{:else if activeItem === 'Add New Poll'}
-		<CreatePollForm />
+		<CreatePollForm on:add={handleAdd}/>
 	{/if}
 </main>
 <Footer/>
